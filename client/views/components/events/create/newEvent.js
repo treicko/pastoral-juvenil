87b3fab651641814b8ed4f1fa32014ccacb1be3b
treicko <leo_mejia_p@hotmail.com>
2017-08-25 07:56:15 -0400

    add form validation to create event

--- client/views/components/events/create/newEvent.js
+++ client/views/components/events/create/newEvent.js
@@ -10,16 +10,22 @@ Template.NewEvent.onRendered(function() {
     key: 'AIzaSyCVKw1zfv0JsOsrH9yeAwoIjwcF7_JDAHY'
   });
 
-  $('.datepicker').pickadate({
+  $('input#event_name').characterCounter();
+
+  const $dateInput = $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
     today: 'Today',
     clear: 'Clear',
     close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
+    closeOnSelect: false, // Close upon selecting a date,
+    min: new Date()
   });
 
-  $('.timepicker').pickatime({
+  const datePicker = $dateInput.pickadate('picker');
+  datePicker.set('select', new Date());
+
+  const $timeInput = $('.timepicker').pickatime({
     default: 'now', // Set default time: 'now', '1:30AM', '16:30'
     fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
     twelvehour: false, // Use AM/PM or 24-hour format
@@ -30,45 +36,31 @@ Template.NewEvent.onRendered(function() {
     ampmclickable: true, // make AM PM clickable
     aftershow: function(){} //Function for after opening timepicker
   });
+
+  document.getElementById('event_hour').value = moment().add(1, 'hours').format('HH:mm');
 });
 
 Template.NewEvent.onCreated(function() {
   const self = this;
-  const eventController = new EventController();
+  //const eventController = new EventController();
+  this.eventController = new ReactiveVar(new EventController());
   this.timeHelper = new ReactiveVar(new TimeHelper());
   
   //var radius = new ReactiveVar(100);
   //var newEventShapeLocation = new ReactiveVar(null);
 
-  GoogleMaps.ready('locationMap', function(map) {
-    eventController.setMap(map);
-    self.autorun(function() {
+  GoogleMaps.ready('locationMap', (map) => {
+    this.eventController.get().setMap(map);
+    self.autorun(() => {
       console.log('onCreated - self.autorun NewEvent'); 
-      eventController.setMapAttributes();
+      this.eventController.get().setMapAttributes();
     });
   });
 });
 
 Template.NewEvent.helpers({
   mapOptions: function() {
-    var latLng = Geolocation.latLng();    
-    if (GoogleMaps.loaded() && latLng) {
-      return {
-        center: new google.maps.LatLng(latLng.lat, latLng.lng),
-        zoom: 15
-      };
-    }
-  },
-  currentDate: function() {
-    var timeHelper = Template.instance().timeHelper.get();
-    console.log('En el helpers...');
-    // console.log('desde el helper perros....', tmp.timeHelper.get().getCurrentDate());
-    return timeHelper.getCurrentDate();    
-  },
-  currentHour: function() {
-    var timeHelper = Template.instance().timeHelper.get();
-    console.log('En el helpers...2');
-    return timeHelper.getCurrentHour();
+    return Template.instance().eventController.get().getMapOptions();
   }
 });
 
@@ -76,18 +68,18 @@ Template.NewEvent.events({
   'submit .new-event': function(event) {
     console.log('esta es mi event compadre: ', event);
     event.preventDefault();
-    
+    const pors = Template.instance().eventController.get().getEventPosition();
+    console.log('NewEvent - SubmitEvent --> lat: ', pors.lat(), ' lng: ', pors.lng());
     const eventDate = new Date(`${event.target.event_date.value} ${event.target.event_hour.value}`)
     const location = { 'latitude': 24, 'longitude': 34 };
-    console.log('mira hermano este es el date: ', eventDate);
-
     const newEvent = {
       'name': event.target.event_name.value,
-      'desc': event.target.event_description.value,
-      'latitude': GoogleMaps.maps.locationMap.instance.getCenter().lat(),
-      'longitude': GoogleMaps.maps.locationMap.instance.getCenter().lng(),
+      'description': event.target.event_description.value,
+      'ubication': event.target.event_ubication.value,
+      'latitude': pors.lat(),
+      'longitude': pors.lng(),
       'map': [{'LEo': 'llalala'}],
-      'date': `${event.target.event_date.value}T${event.target.event_hour.value}`,
+      'date': eventDate,
       'participants': [],
       'comments': [],
       'isDeleted': false,

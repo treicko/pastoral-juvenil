336741d0b9e0cea8292a7380c86e61604ed0aa38
treicko <leo_mejia_p@hotmail.com>
2017-10-30 08:41:50 -0400

    comment to publication

--- client/views/components/groups/publications/groupPublications.js
+++ client/views/components/groups/publications/groupPublications.js
@@ -33,6 +33,7 @@ Template.GroupPublications.onCreated(function() {
 Template.GroupPublications.helpers({
   group: function() {
     const groupFound = Template.instance().groupController.get().getGroupById(groupId);
+    console.log('Group found: ', groupFound);
     if (groupFound) {
       return groupFound;
     }
@@ -62,5 +63,40 @@ Template.GroupPublications.events({
     $("form")[0].reset();
     $('#group_publication_description').trigger('autoresize');
     return false;    
-  }
+  },
+  
+  'keypress .group-publication-comment': function (event, template) {
+    if (event.which === 13) {
+      const newComment = {
+        groupId: document.getElementById("group_publication_groupId").value,
+        publicationId: document.getElementById("group_comment_publicationId").value,
+        userName: document.getElementById("group_publication_userName").value,
+        userId: document.getElementById("group_publication_userId").value,
+        userImage: '',
+        comment: `${event.target.value}`
+      }
+      
+      console.log('Coment: ', event);
+      Template.instance().groupController.get().saveComment(newComment);
+      // event.target.value  = '';
+      // const label = document.getElementById("label_group_publication_comment");
+      // console.log('El form: ', $("#new_comment_publication")[0].reset());
+      // $("#new_comment_publication")[0].reset();
+      // $('#new_comment_publication').trigger("reset");
+      $(':focus').blur();
+      $("#new_comment_publication").focus();
+      // document.getElementById("mytext").focus();
+      Materialize.updateTextFields();
+      $("form")[0].reset();
+      
+      
+      // $("form")[0].reset();
+      // console.log('Labelll: ', label);
+      // $('#label_group-publication-comment').classList.remove("active");
+      // group_publication_comment
+      // $('#group-publication-comment').trigger('autoresize');
+      // event.stopPropagation();
+      return false;
+    }
+  },
 });

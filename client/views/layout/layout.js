ecd745f03046f96557641b6aa2b0b0407a6a8782
treicko <leo_mejia_p@hotmail.com>
2017-11-23 08:45:14 -0400

    add meta on heare and add dropdown to nav

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -2,7 +2,27 @@
 
 Template.layout.onRendered(function() {
   $('.button-collapse').sideNav();
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false, // Stops event propagation
+  });
   document.body.style.backgroundImage = "url('images/bg02.png')";
+  const elems = document.getElementsByClassName('waves-effect waves-light btn');
+  for (let i = 0; i < elems.length; i += 1) {
+    elems[i].removeAttribute('class');
+  }
+  if (elems.length === 1) {
+    elems[0].removeAttribute('class');
+  }
+});
+
+Template.layout.onCreated(function() {
 });
 
 Template.layout.onDestroyed(function () {
